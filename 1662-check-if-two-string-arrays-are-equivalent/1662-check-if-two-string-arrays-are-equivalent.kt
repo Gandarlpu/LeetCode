578c43a026d1108e300c7273c36938be0c933645
Chance <49300186+Gandarlpu@users.noreply.github.com>
2022-02-07 10:58:37 +0900

    Time: 148 ms (96.97%), Space: 35.3 MB (72.73%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.kt
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.kt
@@ -1,30 +1,20 @@
 class Solution {
     fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean {
         
-        var word_1 = ""
-        var word_2 = ""
-        var result = false
+        var word_1 = StringBuilder()
+        var word_2 = StringBuilder()
         
-        for(i in 0..word1.size-1){
-            word_1 += word1[i]
+        word1.forEach{
+            word_1.append(it)
         }
-        for(i in 0..word2.size-1){
-            word_2 += word2[i]
+        word2.forEach{
+            word_2.append(it)
         }
+        println("word_1 : ${word_1}")
+        println("word_2 : ${word_2}")
         
-        if(word_1.length == word_2.length){
-            for(i in 0..word_1.length-1){
-                if(word_1[i] == word_2[i]){
-                    result = true
-                }else{
-                    result = false
-                }
-            }
-            
-        }else{
-            result = false
-        }
+        return word_1.toString() == word_2.toString()
         
-        return result
+        
     }
 }
