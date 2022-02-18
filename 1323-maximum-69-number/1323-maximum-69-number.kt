c07171bfa6bbfbe4c320d1db8ff55198c887369e
Chance <49300186+Gandarlpu@users.noreply.github.com>
2022-02-18 10:49:53 +0900

    Time: 188 ms (55.00%), Space: 34 MB (30.00%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.kt
+++ 1323-maximum-69-number/1323-maximum-69-number.kt
@@ -4,23 +4,12 @@ class Solution {
         var result = StringBuilder()
         result.append(num.toString())
         
-        if(result.length > 1){
-            for(i in 1..result.length-1){
-                if(result[i-1] > result[i]){
-                    result[i] = '9'
-                    break
-                }else if(result[i-1] < result[i]){
-                    result[i-1] = '9'
-                    break
-                }else if(result[i-1] == '6' || result[i] == '6'){
-                    result[i-1] = '9'
-                    break
-                }
+        for(i in 0..result.length-1){
+            if(result[i] == '6'){
+                result[i] = '9'
+                break
             }
-        }else{
-            result[0] = '9'
         }
-        
         
         println("result : ${result}")
     
