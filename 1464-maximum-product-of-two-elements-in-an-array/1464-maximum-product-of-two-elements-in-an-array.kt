cf97c05ce937db4fe882ff50ac249a5a97010eeb
Chance <49300186+Gandarlpu@users.noreply.github.com>
2022-02-21 11:32:03 +0900

    Time: 339 ms (5.26%), Space: 42.2 MB (5.26%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun maxProduct(nums: IntArray): Int {
+        
+        nums.sortDescending()  
+        
+        return (nums[0]-1)*(nums[1]-1)
+        
+        
+    }
+}
