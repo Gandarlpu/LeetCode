12fb872374e54990d747427f05897c8497b22c8b
Chance <49300186+Gandarlpu@users.noreply.github.com>
2022-07-26 21:25:36 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (14.83%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        
+        int[] freq = new int[101];
+        int result = 0;
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            freq[nums[i]]++;
+        }
+        
+        for(int i = 1 ; i < 101 ; i ++){
+            if(freq[i] == 1){
+                result += i;
+            }
+        }
+        
+        return result;
+    }
+}
