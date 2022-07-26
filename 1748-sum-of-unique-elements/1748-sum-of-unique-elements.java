9bcfc4bb311a3dadaba456ee84653b33dc13793b
Chance <49300186+Gandarlpu@users.noreply.github.com>
2022-07-26 21:26:28 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (24.28%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -4,10 +4,13 @@ class Solution {
         int[] freq = new int[101];
         int result = 0;
         
+        // 100개 배열에서 nums에 담긴 요소들의 idx값을 하나씩 늘려줌
         for(int i = 0 ; i < nums.length ; i++){
             freq[nums[i]]++;
         }
         
+        // 늘려준 idx값들이 1이상이면 2개 이상이 있는 것
+    
         for(int i = 1 ; i < 101 ; i ++){
             if(freq[i] == 1){
                 result += i;
